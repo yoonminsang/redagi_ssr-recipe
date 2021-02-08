9f7be5bdf795060e9d61c53cf9f1ab3c3dcc9e29
yoonminsang <apdlyooapdl@naver.com>
2021-02-09 00:11:48 +0900

    ex

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Menu from "./components/Menu";
-import BluePage from "./pages/BluePage";
-import RedPage from "./pages/RedPage";
+// import BluePage from "./pages/BluePage";
+// import RedPage from "./pages/RedPage";
+// import UsersPage from "./pages/UsersPage";
+import loadable from "@loadable/component";
+const RedPage = loadable(() => import("./pages/RedPage"));
+const BluePage = loadable(() => import("./pages/BluePage"));
+const UsersPage = loadable(() => import("./pages/UsersPage"));
 
 const App = () => {
   return (
@@ -11,6 +16,7 @@ const App = () => {
       <hr />
       <Route path="/red" component={RedPage} />
       <Route path="/blue" component={BluePage} />
+      <Route path="/users" component={UsersPage} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,13 +4,55 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import rootReducer, { rootSaga } from "./modules";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleWare from "redux-saga";
+import { loadableReady } from "@loadable/component";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+const sagaMiddleware = createSagaMiddleWare();
+
+const store = createStore(
+  rootReducer,
+  window.__PRELOADED_STATE__, // 이 값을 초기상태로 사용함
+  composeWithDevTools(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(rootSaga);
+
+// 같은 내용을 쉽게 재사용할 수 있도록 렌더링할 내용을 하나의 컴포넌트로 묶음
+const Root = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+const root = document.getElementById("root");
+
+// 프로덕션 환경에서는 loadableReady와 hydrate를 사용하고
+// 개발 환경에서는 기존 방식으로 처리
+if (process.env.NODE_ENV === "production") {
+  loadableReady(() => {
+    ReactDOM.hydrate(<Root />, root);
+  });
+} else {
+  ReactDOM.render(<Root />, root);
+}
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById("root")
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
